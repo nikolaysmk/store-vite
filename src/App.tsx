765aae33fc3a0fb19358a0c00796a2b052f8cf14
Nikolay Smk <nikolaysmyk@icloud.com>
2023-05-01 20:09:27 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,10 @@
 import './App.css';
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from 'react-router-dom';
-
-import { Blog } from './page/Blog';
-import { Dashboard } from './components/Dashboard';
-import { Preferences } from './components/Preferences';
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-      <Route path="/" element={<Blog />} />
-      <Route path="dashboard" element={<Dashboard />} />
-      <Route path="preferences" element={<Preferences />} />
-    </>,
-  ),
-);
+import Routes from './Routes';
 
 function App() {
   return (
     <>
-      <RouterProvider router={router} />
+      <Routes />
     </>
   );
 }
---
+++ src/components/components.tsx
@@ -0,0 +1,15 @@
+export const Header = () => {
+  return (
+    <>
+      <h1>Header</h1>
+    </>
+  );
+};
+
+export const Footer = () => {
+  return (
+    <>
+      <h1>Footer</h1>
+    </>
+  );
+};
